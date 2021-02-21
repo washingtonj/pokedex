6c838b9f0b2972676ab5fe845eee515b95915e6f
Washington Junior <washingtonj@outlook.com.br>
2021-02-21 02:04:06 -0300

    [ABOUT] - Configuração e acrescimo na UI.
    Adicionado imagem de fundo no container do Pokemon, conforme UI.
    
    Criação do container de informações, falta ajustes.

--- src/screens/About/About.js
+++ src/screens/About/About.js
@@ -1,16 +1,29 @@
 import React from "react";
-import { Container, NavigatorHeader, Pokemon } from "./About.styles";
+import {
+  Container,
+  NavigatorHeader,
+  Pokemon,
+  PokemonStates,
+} from "./About.styles";
 import BackIcon from "../../assets/icons/generics/back.svg";
 import { Column, Row } from "../../styles/Container";
 import { Badge } from "../../components";
 
+const PokemonStateContainer = ({ title, value }) => {
+  return (
+    <PokemonStates.Stats.Container>
+      <PokemonStates.Stats.Title children={title} />
+      <PokemonStates.Stats.Value children={value} />
+    </PokemonStates.Stats.Container>
+  );
+};
+
 export default function About({ navigation, route }) {
   const pokemon = route.params.pokemon;
-
-  console.log(pokemon);
+  const layoutColor = pokemon.types[0].toLowerCase();
 
   return (
-    <Container type={pokemon.types[0].toLowerCase()}>
+    <Container type={layoutColor}>
       <NavigatorHeader.Container>
         <NavigatorHeader.Button onPress={() => navigation.goBack()}>
           <BackIcon fill="white" width="25" height="25" />
@@ -18,28 +31,50 @@ export default function About({ navigation, route }) {
       </NavigatorHeader.Container>
 
       <Pokemon.Container>
-        <Pokemon.Image
-          source={{
-            uri: pokemon.sprite,
-          }}
-        />
-        <Column>
-          <Pokemon.Id children={`#00${pokemon.number}`} />
-          <Pokemon.Name children={pokemon.name} />
-          <Row>
-            {pokemon.types.map((type) => (
-              <Badge
-                animation="rubberBand"
-                duration={2500}
-                iterationCount={1}
-                key={type}
-                type={type.toLowerCase()}
-                style={{ marginRight: 8 }}
-              />
-            ))}
-          </Row>
-        </Column>
+        <Pokemon.Wrapper>
+          <Pokemon.Image
+            source={{
+              uri: pokemon.sprite,
+            }}
+          />
+          <Column>
+            <Pokemon.Id children={`#00${pokemon.number}`} />
+            <Pokemon.Name children={pokemon.name} />
+            <Row>
+              {pokemon.types.map((type) => (
+                <Badge
+                  animation="rubberBand"
+                  duration={2500}
+                  iterationCount={1}
+                  key={type}
+                  type={type.toLowerCase()}
+                  style={{ marginRight: 8 }}
+                />
+              ))}
+            </Row>
+          </Column>
+        </Pokemon.Wrapper>
       </Pokemon.Container>
+      <PokemonStates.Container>
+        <PokemonStates.Description children={pokemon.description} />
+
+        <Column style={{ marginTop: 30 }}>
+          <PokemonStates.Title children="Pokemon Data" color={layoutColor} />
+          <PokemonStateContainer title="Species" value={pokemon.species} />
+          <PokemonStateContainer title="Height" value={pokemon.height} />
+          <PokemonStateContainer title="Weight" value={pokemon.weight} />
+          <PokemonStateContainer title="Abilities" />
+          <PokemonStateContainer title="Weaknesses" />
+        </Column>
+        <Column style={{ marginTop: 30 }}>
+          <PokemonStates.Title children="Pokemon Data" color={layoutColor} />
+          <PokemonStateContainer title="Species" value={pokemon.species} />
+          <PokemonStateContainer title="Height" value={pokemon.height} />
+          <PokemonStateContainer title="Weight" value={pokemon.weight} />
+          <PokemonStateContainer title="Abilities" />
+          <PokemonStateContainer title="Weaknesses" />
+        </Column>
+      </PokemonStates.Container>
     </Container>
   );
 }
--- src/screens/About/About.styles.js
+++ src/screens/About/About.styles.js
@@ -1,13 +1,20 @@
 import styled from "styled-components/native";
 import { Colors } from "../../styles/Colors";
-import { FilterTitle, ApplicationTitle } from "../../styles/Typography";
-import { Image } from 'react-native-animatable'
+import {
+  FilterTitle,
+  ApplicationTitle,
+  Description,
+  PokemonType,
+} from "../../styles/Typography";
+import { Image } from "react-native-animatable";
+import { View } from "react-native-animatable";
+import BackgroundImage from "../../assets/icons/generics/about-bg.png";
 
 export const Container = styled.View`
   flex: 1;
   flex-direction: column;
   background-color: ${({ type }) => Colors[type].bg};
-  padding: 20px 0;
+  padding: 20px 0 0 0;
 `;
 
 export const NavigatorHeader = {
@@ -25,16 +32,24 @@ export const NavigatorHeader = {
 };
 
 export const Pokemon = {
-  Container: styled.View`
+  Container: styled.ImageBackground.attrs({
+    source: BackgroundImage,
+    imageStyle: { resizeMode: "cover", position: "absolute", left: 50 },
+  })`
+    width: 100%;
+    margin-top: 50px;
+    margin-bottom: 80px;
+  `,
+
+  Wrapper: styled.View`
     display: flex;
     flex-direction: row;
     justify-content: center;
     align-items: center;
-    margin-top: 50px;
   `,
 
   Image: styled(Image).attrs({
-    animation: 'fadeInLeft'
+    animation: "fadeInLeft",
   })`
     width: 125px;
     height: 125px;
@@ -42,17 +57,55 @@ export const Pokemon = {
   `,
 
   Id: styled(FilterTitle).attrs({
-    animation: 'lightSpeedIn',
-    duration: 1000
+    animation: "lightSpeedIn",
+    duration: 1000,
   })`
     color: ${Colors.text.number};
   `,
 
   Name: styled(ApplicationTitle).attrs({
-    animation: 'lightSpeedIn',
-    duration: 1100
+    animation: "lightSpeedIn",
+    duration: 1100,
   })`
     color: ${Colors.text.white};
     margin-bottom: 8px;
   `,
+};
+
+export const PokemonStates = {
+  Container: styled(View).attrs({
+    animation: "fadeInUp",
+  })`
+    background: white;
+    flex: 1;
+    border-top-left-radius: 35px;
+    border-top-right-radius: 35px;
+    padding: 40px;
+  `,
+
+  Description: styled(Description)`
+    color: ${Colors.text.gray};
+  `,
+
+  Title: styled(FilterTitle)`
+    color: ${(props) => Colors[props.color].type};
+    margin-bottom: 16px;
+  `,
+
+  Stats: {
+    Container: styled.View`
+      display: flex;
+      flex-direction: row;
+      align-items: center;
+      margin-bottom: 16px;
+    `,
+
+    Title: styled(PokemonType)`
+      width: 35%;
+    `,
+
+    Value: styled(Description)`
+      color: ${Colors.text.gray};
+    `,
+  },
 };
